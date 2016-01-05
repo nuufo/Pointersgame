f4ced70e3879af79dfde56abec442df6a0477ce6
karl karlsson <karl karlsson>
2016-01-05 18:56:02 +0100

    download

--- Pointgame.sql
+++ Pointgame.sql
@@ -1,12 +1,12 @@
--- Adminer 4.2.2 MySQL dump
+-- Adminer 4.2.3 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `todo_list`;
-CREATE TABLE `todo_list` (
+DROP TABLE IF EXISTS `todo-list`;
+CREATE TABLE `todo-list` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `task` text COLLATE utf8_bin NOT NULL,
   `point` int(11) NOT NULL,
@@ -28,4 +28,4 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `password`, `firstname`, `lastname`, `todo_id`) VALUES
 (1,	'test',	'test',	'testmannen',	'ahsdashd',	1);
 
--- 2015-12-28 09:54:32
+-- 2015-12-27 09:57:03
