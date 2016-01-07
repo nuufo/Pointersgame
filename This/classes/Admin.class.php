dadb7d70595744c88ffb413d66af35e6daeadc18
Patrik Nordahl <Patrik Nordahl>
2016-01-07 13:05:58 +0100

    provar igen

--- This/classes/Admin.class.php
+++ This/classes/Admin.class.php
@@ -1,52 +1,9 @@
 <?php
 class Admin{
 
-	public static function login($params){
+	public static function hemsida($params){
 		
-		if(isset($_POST['login'])){
-
-			$mysqli = DB::getInstance();
-			$username = $mysqli->real_escape_string($_POST['username']);
-			$password = $mysqli->real_escape_string($_POST['password']);
-
-			//$password = crypt($password,'$2a$'.sha1($username));
-
-			$query = "
-				SELECT id, username
-				FROM user
-				WHERE username = '$username'
-				AND password = '$password'
-				LIMIT 1
-			";
-
-			$result = $mysqli->query($query);
-			$user = $result->fetch_assoc();
-
-
-	
-	public static function create($params){
-
-		if(isset($_POST['createItem'])){
-			$mysqli = DB::getInstance();
-			$title = $mysqli->real_escape_string($_POST['createItem']);
-		
-		
-			$query = "
-				INSERT INTO todolist 
-				(title) 
-				VALUES (".$$title.")
-			";
-
-			$mysqli->query($query);
-
-			return ['newpost' => FALSE, 'message' => 'Nytt inlägg skapat!'];
-		}
-
-		return ['newpost' => TRUE];
-	}
-
-
-	
-
+		return [ 'hemsida'];	
+ }
 
 }
