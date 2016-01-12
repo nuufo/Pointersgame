fb1e7fab62871885a3aaaeaa30ce3185c167ccb6
Patrik Nordahl <Patrik Nordahl>
2016-01-12 19:43:31 +0100

    Added everything/Pat

--- This/classes/Todolist.class.php
+++ This/classes/Todolist.class.php
@@ -2,63 +2,8 @@
 <?php
 	
 class Todolist{
-#16. Här börjar methoden som vi är ute efter i URL:en.  /Posts/all
-
-public static function getUserlist($params) {
- 	$mysqli = DB::getInstance();
-			$query1 = " SELECT * FROM todolist, user,listitem
-				WHERE user.id = todolist.user_id AND todolist.user_id = listitem.todo_id AND user.id= ".$_SESSION['user']['id']." ";
-
-			$result1 = $mysqli->query($query1);
-
-		 		while($listitem = $result1->fetch_assoc()){
-		 		$listitems[] = $listitem;
-			
-			}
-			 return [ 'listitems' => $listitems];
-
-}
 
 
-	public static function all($params){
-
-	 $mysqli = DB::getInstance();
-		 $result = $mysqli->query("SELECT * FROM todolist");
-
-		 while($todolist = $result->fetch_assoc()){
-		 	$todolists[] = $todolist;
-		 	
-		 }
-		 #18. Queryn körs mot databasen och vi väljer nedan att returnera något som vi kallar för 'post'. 
-		#Denna 'post' är kopplat till Twig. Så när denna return körs returneras värdet 'posts' tillbaka till index.php
-		 #Gå tillbaka till index.php och följ punkt #19.
-		 echo "skräp skall komma nedanför";
-		 return ['todolists' => $todolists,
-		 			//'redirect' => '?/Todolist/userList'
-		 ];
-
-		
-	}
-
-	public static function create($params){
-
-		if(isset($_POST['createItem'])){
-			$mysqli = DB::getInstance();
-			$listname = $mysqli->real_escape_string($_POST['createItem']);
-			
-
-			$query = "
-				INSERT INTO listitem 
-				(task, todo_id) 
-				VALUES ('$listname', ".$_SESSION['user']['id'].")
-			";
-
-			$mysqli->query($query);
-
-			return ['redirect' =>  '?/Todolist/getUserlist']; //];
-		}
-
-	}
 
 	public static function createtodolist($params){
 
@@ -75,9 +20,64 @@ public static function getUserlist($params) {
 
 			$mysqli->query($query);
 
-			return ['redirect' =>  '?/Todolist/create']; //];
+			return ['redirect' =>  '?/Todolist/all']; //];
 		}
 
 	}
+
+	public static function createlistitem($params){
+
+		if(isset($_POST['createItem'])){
+			$mysqli = DB::getInstance();
+			$task = $mysqli->real_escape_string($_POST['createItem']);
+			$score= $mysqli->real_escape_string($_POST['createpoints']);
+			$post_id = $mysqli->real_escape_string($_POST['todolist_id']);
+
+			$query = "
+				INSERT INTO listitem
+				(task, score, todolist_id) 
+				VALUES ('$task', '$score', $post_id)
+			";
+
+			$mysqli->query($query);
+
+			return ['redirect' => $_SERVER['HTTP_REFERER']];
+		}
+
+	}
+
+	public static function single($params){
+		
+		$id = $params[0];
+		$mysqli = DB::getInstance();
+		$id = $mysqli->real_escape_string($id);
+		$result = $mysqli->query("SELECT * FROM todolist WHERE id=$id");
+		$todolist = $result->fetch_assoc();
+
+		$result = $mysqli->query("SELECT * FROM listitem WHERE todolist_id=$id");
+		
+		while($listitem = $result->fetch_assoc()){
+			$listitems[] = $listitem;
+		}	 	
+
+	 	return ['todolist' => $todolist, 'listitems' => $listitems];  
+		
+	}
+		public static function all($params){
+				#17. Värdet som kommer ut här som $params är $url_parts som vi skickade in från index.php. ($params kan heta vad somhelst.)
+		 $mysqli = DB::getInstance();
+		 $result = $mysqli->query(" SELECT * FROM todolist  ");
+
+		 while($todolist = $result->fetch_assoc()){
+		 	$todolists[] = $todolist;
+		 }
+		 #18. Queryn körs mot databasen och vi väljer nedan att returnera något som vi kallar för 'post'. 
+		#Denna 'post' är kopplat till Twig. Så när denna return körs returneras värdet 'posts' tillbaka till index.php
+		 #Gå tillbaka till index.php och följ punkt #19.
+		 return ['todolists' => $todolists];
+		
+	}
+
+
 
 }
