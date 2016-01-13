486427df0992d48c48c98967c404e29b28926867
karl karlsson <karl karlsson>
2016-01-13 11:58:58 +0100

    Styled About-pages. Cleaned upped index.php

--- This/classes/generic.class.php
+++ This/classes/generic.class.php
@@ -1,19 +1,13 @@
 
 <?php
 class generic {
-
 	public static function aboutGoatTeam($params){
-		
-
 		return ['template' => 'aboutGoatTeam.html'];
 
+}
 
-	}
 	public static function aboutPointGame($params){
-		
-
 		return ['template' => 'aboutPointGame.html'];
-
 
 	}
 }
